e0f063f59c52b58048d79c833f34fdc895b73815
dheeraj0007 <dheeraj1863.be20@chitkara.edu.in>
2025-05-01 12:30:41 +0530

    apos added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl text-center flex justify-center items-center h-screen">
-        Dheeraj Gulati's Portfolio
+        Dheeraj Gulati&apos;s Portfolio
       </h1>
     </div>
   );
